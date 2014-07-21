66bf0876affce1c4ec3c551a7e9fa31be7a09a6a
Samsoe <samsoe@Erik-Samsoes-MacBook-Pro.local>
2014-07-21 10:50:38 -0600

    Map Control Position Update

--- js/map.js
+++ js/map.js
@@ -3,7 +3,26 @@ function initialize() {
 	var mapOptions = {
 		zoom: 9,
 		center: new google.maps.LatLng(46.692, -114.015),
-		mapTypeId: google.maps.MapTypeId.TERRAIN
+		mapTypeId: google.maps.MapTypeId.TERRAIN,
+		mapTypeControl: true,
+	    mapTypeControlOptions: {
+	        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+	        position: google.maps.ControlPosition.TOP_CENTER
+	    },
+	    panControl: true,
+	    panControlOptions: {
+	        position: google.maps.ControlPosition.RIGHT_TOP
+	    },
+	    streetViewControl: true,
+	    streetViewControlOptions: {
+	        position: google.maps.ControlPosition.RIGHT_TOP
+	    },
+	    zoomControl: true,
+	    zoomControlOptions: {
+	        style: google.maps.ZoomControlStyle.LARGE,
+	        position: google.maps.ControlPosition.RIGHT_TOP
+	    },
+	    scaleControl: true
 	};
 
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
